23b373cc5d700894d1c487487419235baf5016e2
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-05-13 13:03:13 +0300

    start databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-servers',
+  selector: 'app-servers',
   // selector: '[app-servers]',
-  selector: '.app-servers',
-  template: `
-            <app-server></app-server>
-            <app-server></app-server>
-            `,
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
