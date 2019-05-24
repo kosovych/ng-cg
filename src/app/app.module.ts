c9515751d86a94bbfcebbe23f484769b28b1c31c
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-05-24 11:54:01 +0300

    fixed rpoject structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './app/header/header.component';
-import { RecipteBookComponent } from './app/recipte-book/recipte-book.component';
-import { ShopingEditComponent } from './app/shoping-list/shoping-edit/shoping-edit.component';
-import { RecipteListComponent } from './app/recipte-book/recipte-list/recipte-list.component';
-import { RecipteItemComponent } from './app/recipte-book/recipte-list/recipte-item/recipte-item.component';
-import { RecipteDetailsComponent } from './app/recipte-book/recipte-details/recipte-details.component';
-import { ShopingSistComponent } from './app/shoping-list/shoping-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipteBookComponent } from './recipte-book/recipte-book.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
+import { RecipteListComponent } from './recipte-book/recipte-list/recipte-list.component';
+import { RecipteItemComponent } from './recipte-book/recipte-list/recipte-item/recipte-item.component';
+import { RecipteDetailsComponent } from './recipte-book/recipte-details/recipte-details.component';
+import { ShopingSistComponent } from './shoping-list/shoping-list.component';
 
 @NgModule({
   declarations: [
