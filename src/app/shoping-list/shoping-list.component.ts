f630922421afc7dbd3ee1c2013ac4989a5d958f3
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-06-10 12:56:24 +0300

    added bassic structure

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
 
 @Component({
   selector: 'app-shoping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shoping-list.component.scss']
 })
 export class ShopingSistComponent implements OnInit {
+  ingridients : Ingridient[] = [
+    new Ingridient('Apples', 5),
+    new Ingridient('Tomates', 15),
+  ];
 
   constructor() { }
 
