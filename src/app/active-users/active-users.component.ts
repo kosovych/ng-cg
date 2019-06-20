d1528c3597ce93d6fc77886f9848bbba23348ac6
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-06-20 12:55:59 +0300

    added user service

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { UserService } from '../shared/services/user.service';
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
+export class ActiveUsersComponent implements OnInit{
   @Output() userSetToInactive = new EventEmitter<number>();
+  public users: string[] = [];
+
+  constructor(
+    private userService: UserService,
+  ) {}
+
+  ngOnInit() {
+    this.users = this.userService.activeUsers;
+  }
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.userService.onSetToInactive(id);
   }
 }
