62ede1f3447b1ed765a24398fb0d3ada43fbe60c
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-06-21 09:30:38 +0300

    added ing

--- src/app/recipte-book/recipe.service.ts
+++ src/app/recipte-book/recipe.service.ts
@@ -1,25 +1,50 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Recipe } from './recipe.model';
+import {
+  Injectable,
+  EventEmitter
+} from '@angular/core';
+import {
+  Recipe
+} from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter < Recipe > ();
   private recipes: Recipe[] = [
     new Recipe(
       'Chicken',
       'Some tasty chicken',
       'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/04/one-pot-poached-chicken.jpg?itok=R3NMZk-d',
-      ),
-      new Recipe(
-        'Chicken 2',
-        'Some tasty chicken 2',
-        'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/04/one-pot-poached-chicken.jpg?itok=R3NMZk-d',
-        ),
+      [
+        {
+          name: 'chicken',
+          amount: 1,
+        },
+        {
+          name: 'lemon',
+          amount: 0.5,
+        },
+    ],
+    ),
+    new Recipe(
+      'Chicken 2',
+      'Some tasty chicken 2',
+      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/04/one-pot-poached-chicken.jpg?itok=R3NMZk-d',
+      [
+        {
+          name: 'chicken',
+          amount: 2,
+        },
+        {
+          name: 'lemon',
+          amount: 1,
+        },
+    ],
+    ),
   ]
 
-  constructor() { }
+  constructor() {}
 
   getRecipes() {
     return this.recipes.slice();
