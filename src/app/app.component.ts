eed8b50c618a2d23103da1630ed6c887e67f4123
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-06-26 23:59:38 +0300

    start form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: true}) form: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  onSubmit(form: NgForm) {
+    console.log(this.form);
+  }
+
+
+
 }
