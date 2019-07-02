23d0a6d13b8b4713e4cc411dec549ccc71a14ee9
Yaroslav Kosovych <bassicinbox@gmail.com>
2019-07-02 13:12:31 +0300

    added group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', {static: true}) form: NgForm;
+  defaultValue = 'pet';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
